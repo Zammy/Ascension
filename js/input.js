0f2b4535ebf76de22528da26e06ee32c47ad6ffb
Kostadin Vodenicharov <kostadinvo@gmail.com>
2016-01-30 19:45:59 +0200

    Touch capability.

--- js/input.js
+++ js/input.js
@@ -2,4 +2,8 @@ function addMouseHandler() {
 	$("canvas").on("mousedown", function(event) {
 		playerClickedOn( realToMapPos({x: event.offsetX, y: event.offsetY}) );
 	});
+	$("canvas").on("touchstart", function(event) {
+		var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
+		playerClickedOn( realToMapPos({x: touch.clientX, y: touch.clientY}) );
+	});
 }
