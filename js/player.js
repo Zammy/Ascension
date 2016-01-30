8b010c7cd5d76a74d058302275b2071dc59c65e6
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-01-30 15:37:55 +0200

    Rotation works

--- js/player.js
+++ js/player.js
@@ -5,8 +5,37 @@ function playerGoTo(pos) {
 	}
 }
 
+
+function calcActorRot(actor) {
+	var playerMapPos = getActorMapPos(actor);
+	var dirVec = pointSubtract(actor.next, playerMapPos);
+	if (dirVec.x < 0) {
+		actor.dir = 'w';
+		actor.container.rotation =  Math.PI*1.5;
+	} else if (dirVec.x > 0) {
+		actor.dir = 'e';
+		actor.container.rotation =  Math.PI/2;
+	} else if (dirVec.y > 0) {
+		actor.dir = 's';
+		actor.container.rotation =  Math.PI;
+	} else {
+		actor.dir = 'n';
+		actor.container.rotation =  0;
+	}
+}
+
 function getActorMapPos(actor) {
-	return realToMapPos( actor.container.position );
+	// if (actor.next) {
+		// return mapToRealPos( actor.next );
+	// } else {
+		return realToMapPos( actor.container.position );
+	// }
+}
+
+function setNext(actor) {
+	var playerMapPos = getActorMapPos(actor);
+	actor.next = find_path(playerMapPos.x, playerMapPos.y, actor.goal.x, actor.goal.y);
+	calcActorRot(actor);
 }
 
 function updateActor(actor, now) {
@@ -15,9 +44,8 @@ function updateActor(actor, now) {
 	}
 	var playerMapPos = getActorMapPos(actor);
 	if (!actor.next) {
-		actor.next = find_path(playerMapPos.x, playerMapPos.y, actor.goal.x, actor.goal.y);
+		setNext(actor);
 	}
-	var playerMapPos = getActorMapPos(actor);
 	var realNext = mapToRealPos( actor.next );
 	var delta = pointSubtract(realNext, actor.container.position);
 	normalize(delta)
@@ -34,6 +62,6 @@ function updateActor(actor, now) {
 		actor.goal = null;
 		actor.next = null;
 	} else {
-		actor.next = find_path(playerMapPos.x, playerMapPos.y, actor.goal.x, actor.goal.y);
+		setNext(actor);
 	}
 }
