f3096e8e51c50568fc9dff9b27e62ef0bb244906
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-01-31 15:28:14 +0200

    Level2 done

--- levels/level2.js
+++ levels/level2.js
@@ -1,32 +1,53 @@
 levels[2] = 
 {
-	"player" : { "x" : 0, "y" : 1},
+	"player" : { "x" : 4, "y" : 4},
 	"guards" :
 	[
 		{
-			"x" : 12, 
-			"y" : 1, 
+			"x" : 16, 
+			"y" : 3, 
 			"dir" : "e",
 			"routine" : [
-				["walk", 16, 2],
-				["dir", "e"],
+				["walk", 16, 3],
+				["walk", 16, 1],
+				["walk", 19, 1],
+				["walk", 19, 3]
+			]
+		},		
+		{
+			"x" : 12, 
+			"y" : 2, 
+			"dir" : "e",
+			"routine" : [
+				["walk", 12, 8],
+				["dir", "w"],
 				["wait", 3000],
-				["walk", 16, 4],
-				["walk", 12, 3],
-				["walk", 12, 1]
-			]}
+				["walk", 12, 2]
+			]
+		},	
+		{
+			"x" : 0, 
+			"y" : 10, 
+			"dir" : "e",
+			"routine" : [
+				["walk", 4, 10],
+				["dir", "n"],
+				["wait", 3000],
+				["walk", 0, 10]
+			]
+		},
 	],
 	"map" :
 	[
 		[ "w", "w" , "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "D", "w" , "w", "w", "w", "w"],
 		[ "o", "o" , "o", "o", "o", "o", "o", "o", "o", "o", "w", "w", "w", "w", "o", "w" , "o", "o", "o", "o"],
-		[ "w", "w" , "w", "w", "w", "o", "w", "w", "w", "o", "o", "o", "o", "w", "o", "w" , "o", "w", "w", "o"],
-		[ "o", "w" , "o", "w", "o", "o", "o", "o", "w", "o", "w", "w", "o", "w", "o", "o" , "o", "o", "o", "s"],
-		[ "o", "o" , "o", "w", "w", "o", "w", "o", "w", "o", "w", "o", "o", "w", "o", "w" , "w", "w", "w", "o"],
-		[ "o", "w" , "o", "w", "o", "w", "w", "w", "w", "o", "w", "w", "o", "w", "o", "w" , "o", "o", "o", "s"],
+		[ "w", "w" , "w", "w", "w", "s", "w", "w", "w", "o", "o", "o", "o", "w", "o", "w" , "o", "w", "w", "o"],
+		[ "o", "w" , "o", "w", "w", "o", "o", "o", "w", "o", "w", "w", "o", "w", "o", "o" , "o", "o", "o", "o"],
+		[ "o", "s" , "o", "w", "o", "o", "w", "o", "w", "o", "w", "o", "o", "w", "o", "w" , "w", "w", "w", "o"],
+		[ "o", "w" , "o", "w", "w", "w", "w", "w", "w", "o", "w", "w", "o", "w", "o", "w" , "o", "o", "o", "s"],
 		[ "o", "w" , "o", "o", "o", "o", "o", "o", "o", "o", "o", "w", "o", "w", "o", "w" , "o", "w", "w", "o"],
 		[ "o", "w" , "o", "w", "o", "w", "w", "w", "w", "w", "w", "w", "o", "w", "w", "o" , "s", "w", "o", "o"],
-		[ "o", "w" , "o", "w", "o", "o", "o", "w", "w", "o", "o", "o", "o", "w", "o", "s" , "w", "w", "w", "o"],
+		[ "o", "w" , "o", "w", "s", "o", "o", "w", "w", "o", "o", "o", "o", "w", "o", "s" , "w", "w", "w", "o"],
 		[ "o", "w" , "w", "w", "o", "w", "o", "o", "o", "o", "w", "o", "w", "w", "o", "w" , "w", "s", "o", "o"],
 		[ "o", "o" , "o", "o", "o", "w", "w", "w", "w", "o", "w", "o", "o", "o", "s", "o" , "w", "a", "w", "w"]
 	],
@@ -39,8 +60,8 @@ levels[2] =
 			"type" : "altar", 
 			"passable" : false,
 			"sprites" : {
-				"notVisited" : "altar.png",
-				"visited" : "altar_visited.png"
+				"notVisited" : "altar-0.png",
+				"visited" : "altar-3.png"
 			}
 		},
 		"s" :
@@ -59,8 +80,8 @@ levels[2] =
 			"type" : "door",
 			"passable" : false,
 			"sprites" : {
-				"open" : "door_open.png",
-				"close" : "door_close.png"
+				"open" : "door-1.png",
+				"close" : "door-0.png"
 			}
 		}
 	}
