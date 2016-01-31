fa9208cd5f264b437260113e5b0b21706a1ed663
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-01-31 14:01:18 +0200

    Level 9 mother fuckers!

---
+++ levels/level9.js
@@ -0,0 +1,90 @@
+levels[9] = 
+{
+	"player" : { "x" : 0, "y" : 9},
+	"guards" :
+	[
+		{
+			"x" : 1, 
+			"y" : 3, 
+			"dir" : "e",
+			"routine" : [
+				["walk", 3, 3],
+				["walk", 3, 5],
+				["walk", 1, 5],
+				["walk", 1, 3]
+			]
+		},
+		{
+			"x" : 6, 
+			"y" : 4, 
+			"dir" : "e",
+			"routine" : [
+				["walk", 9, 4],
+				["walk", 9, 7],
+				["walk", 6, 7],
+				["walk", 6, 4]
+			]
+		},
+		{
+			"x" : 2, 
+			"y" : 1, 
+			"dir" : "e",
+			"routine" : [
+				["walk", 8, 1],
+				["dir", "e"],
+				["wait", 5000],
+				["walk", 2, 1],
+				["dir", "s"],
+				["wait", 5000],
+			]
+		}
+	],
+	"map" :
+	[												   //10
+		[ "w", "w" , "w", "w", "w", "w", "w", "w", "w", "w", "a", "w", "w", "w", "w", "w" , "w", "w", "w", "w"],
+		[ "w", "w" , "o", "o", "o", "o", "o", "o", "o", "s", "o", "s", "o", "s", "s", "s" , "s", "w", "w", "D"],
+		[ "w", "w" , "o", "w", "o", "w", "o", "w", "w", "w", "o", "w", "o", "w", "w", "w" , "o", "o", "o", "o"],
+		[ "w", "o" , "o", "o", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" , "w", "w", "o", "w"],
+		[ "w", "o" , "w", "o", "o", "o", "o", "o", "o", "o", "w", "w", "w", "w", "w", "w" , "w", "w", "o", "w"],
+		[ "w", "o" , "o", "o", "w", "w", "o", "w", "w", "o", "w", "w", "w", "o", "o", "o" , "s", "s", "s", "w"],
+		[ "w", "w" , "o", "w", "w", "w", "o", "w", "w", "o", "w", "w", "w", "o", "w", "o" , "w", "w", "o", "w"],
+		[ "w", "w" , "o", "w", "w", "w", "o", "o", "o", "o", "w", "w", "w", "o", "w", "o" , "o", "o", "o", "w"],
+		[ "w", "w" , "o", "o", "o", "w", "w", "w", "w", "s", "w", "w", "w", "o", "w", "w" , "w", "w", "o", "w"],
+		[ "o", "o" , "o", "w", "o", "w", "o", "o", "o", "o", "o", "o", "o", "o", "o", "w" , "w", "w", "w", "w"],
+		[ "w", "o" , "w", "w", "o", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" , "w", "w", "w", "w"]
+	],
+	"defenition" : 
+	{
+		"w" : { "type" : "wall", "sprite" : "wall1.png" , "passable" : false },
+		"o" : { "type" : "floor", "sprite" : "ground1.png" , "passable" : true },
+		"a" : 
+		{
+			"type" : "altar", 
+			"passable" : false,
+			"sprites" : {
+				"notVisited" : "altar.png",
+				"visited" : "altar_visited.png"
+			}
+		},
+		"s" :
+		{
+			"type" : "spikes",
+			"passable" : true,
+			"sprites" : {
+				"floor" : "ground2.png",
+				"hidden" : "bloodyTrap-00.png",
+				"shown" : "bloodyTrap-01.png"
+				}
+   
+		},
+		"D" :
+		{
+			"type" : "door",
+			"passable" : false,
+			"sprites" : {
+				"open" : "door_open.png",
+				"close" : "door_close.png"
+			}
+		}
+	}
+};
